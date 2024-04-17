b3ae8195f412a719d709ef0cd2fac842e8092ba5
Sanjay shitole <scshitole@gmail.com>
2024-04-17 16:36:04 -0700

    Create nginxplus.tf

---
+++ terraformfiles/nginxplus.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "nginx-plus" {
+  count = 2
+  ami                    = "ami-0f40f091219a2e61f"
+  instance_type          = "c5.large"
+  subnet_id              = module.vpc.public_subnets[0]
+  vpc_security_group_ids = [aws_security_group.f5.id]
+  iam_instance_profile   = aws_iam_instance_profile.scsprofile.name
+  key_name               = aws_key_pair.demo.key_name
+  tags = {
+    Name = "${var.prefix}-nginxplus-${count.index}"
+    Env  = "scs-env"
+  }
+}
+output "To_SSH_nginx-plus" {
+  value = [
+    "ssh -i ${aws_key_pair.demo.key_name}.pem ec2-user@${aws_instance.nginx-plus.0.public_ip}",
+    "ssh -i ${aws_key_pair.demo.key_name}.pem ec2-user@${aws_instance.nginx-plus.1.public_ip}"
+  ]
+}
