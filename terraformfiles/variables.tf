82999b173ec04594d4ff88a979618812e4fc0cd4
Sanjay shitole <scshitole@gmail.com>
2024-04-17 16:38:10 -0700

    Create variables.tf

---
+++ terraformfiles/variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  description = "provide some prefix for deployment"
+  default = "scs-nginx"
+}
+
+variable "region" {
+  description = "AWS region you can define example is us-west-2 "
+ default = "us-west-2"
+}
+
+
+variable "allow_from" {
+  description = "IP Address/Network to allow traffic from your machine (i.e. 192.0.2.11/32)"
+default = "73.254.158.216/32"
+}
